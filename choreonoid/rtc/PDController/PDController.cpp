f9c5d8da5660d9586c43c0b4181ca4157aab5034
takayan660 <s1526084PQ@s.chibakoudai.jp>
2019-05-27 07:34:02 +0900

    Compatible with Choreonoid 1.7

--- choreonoid/rtc/PDController/PDController.cpp
+++ choreonoid/rtc/PDController/PDController.cpp
@@ -77,7 +77,7 @@ void PDController::ReadGain(size_t numJoints,std::vector<double> &pgain,std::vec
     FILE *PDGain;
     float temp_pgain,temp_dgain;
     char joint_name[20];
-    PDGain = fopen("/usr/lib/choreonoid-1.6/rtc/pdgain.txt","r");
+    PDGain = fopen("/usr/local/lib/choreonoid-1.7/rtc/pdgain.txt","r");
 
     pgain.clear(); dgain.clear();
 
